cbd25d91008f1fd7e6e214e82e0f64bd142170d0
Hoa-Vo <dev.h1620@gmail.com>
2021-01-11 21:10:41 +0700

    order detail page

--- ViewModels/BillPageViewModel.cs
+++ ViewModels/BillPageViewModel.cs
@@ -1,10 +1,13 @@
-﻿using CakeShop_WPfApp.Models;
+﻿using CakeShop_WPfApp.Commands;
+using CakeShop_WPfApp.Models;
 using CakeShop_WPfApp.Services;
+using CakeShop_WPfApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CakeShop_WPfApp.ViewModels
 {
@@ -13,10 +16,17 @@ namespace CakeShop_WPfApp.ViewModels
         private MainViewModel mainViewModel;
         public List <OrderModel> OrderList { get; set; }
         public OrderServices OrderServices = new OrderServices();
+        public ICommand GotoDetail { get; set; }
         public BillPageViewModel(MainViewModel param)
         {
             mainViewModel = param;
             OrderList = OrderServices.LoadAllOrder();
+            GotoDetail = new RelayCommand(o => GotoDetailPage(o));
+        }
+        public void GotoDetailPage(object parameter)
+        {
+            var DetailWindow = new DetailBilPage(int.Parse(parameter.ToString()));
+            DetailWindow.Show();
         }
     }
 }
---
+++ ViewModels/DetailBillPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CakeShop_WPfApp.Models;
+using CakeShop_WPfApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeShop_WPfApp.ViewModels
+{
+    public class DetailBillPageViewModel:BaseViewModel
+    {
+        public OrderModel Order { get; set; }
+        public OrderServices OrderServices = new OrderServices();
+        public DetailBillPageViewModel(int id)
+        {
+            Order = OrderServices.LoadSingleOrder(id);
+        }
+    }
+}
